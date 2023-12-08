31a55b17a5aa6e74dad7e9378a91249b73abffdf
Armanium <armanium2000@gmail.com>
2023-12-08 22:48:33 +0400

    Revert "assets"
    This reverts commit d004497717c3b8ed1209cd2b32dc7fd808344ac2.

--- Assets/Scripts/Enemy/Behaviour/Interfaces/IRunable.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IRunable
-{
-    void Move(Transform transform, float speed, Vector3 destionation);
-    List<Tile> GetMovePath(Tile origin, Tile destination);
-}
